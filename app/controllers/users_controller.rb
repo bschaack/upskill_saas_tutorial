8b47a7efdcbfe4b8de94652897c9150b269fd1e1
Brad Schaack <bschaack@gmail.com>
2018-05-23 23:08:21 +0000

    Added before filters to check correct user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
- 
+  # Secures users so you have to be logged in to view
+  before_action :authenticate_user!
+  
   # GET to /users/:id
   def show
     @user = User.find( params[:id] )
